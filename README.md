81726cf8fc9e9dcb2e73a42682154ba3a17e9261
Daniel Ziltener <dziltener@lyrion.ch>
2013-01-08 02:35:53 +0100

    There you go!

--- README.md
+++ README.md
@@ -1,13 +1,111 @@
-# visuflow
+# VisuFlow
 
-A Clojure library designed to ... well, that part is up to you.
+A Clojure library designed to help you organize the order of your functions.
 
 ## Usage
 
-FIXME
+### Ingredients - a small overview...
+For this to work, you need functions you want to link together.
+
+Then you need some kind of "state" you want to pass through.
+
+Most likely you'll need a map of *validargs*.
+Validargs are keywords referring to a "testing scheme". More on that later.
+
+Finally, you need a nested structure describing what you want to do.
+
+### The nested structure
+The nested structure describes how your data flows through your functions:
+Lists hold everything together:
+```clojure
+'(first [:!f identity :true] ([println true]) ([println false]))
+```
+If a list entry consists of more than one thing, you wrap them in a vector:
+```clojure
+[:!fp #(inc %) :data]
+```
+Finally, a sub-structure is again a list:
+```clojure
+(:mapkey)
+```
+
+### Calling functions and keywords - The result stack
+VisuFlow tries to intelligently use the args your embedded function takes.
+First, an important thing: Throughout a walk, VisuFlow keeps all results in a stack-like structure.
+That is implemented as a Clojure list using conj, take and drop.
+
+At first, the stack only contains the "state" you passed in. Then, after each function call
+the result gets put on top of it.
+
+*What has this to do with the arity of your functions?*
+That's simple: Passing in a one-arg function, VisuFlow hands it the topmost value of the stack.
+Passing in a two-arg function, it gets the two topmost args, and so on. Until you run out of stack.
+
+#### The :!p keyword
+Look at this example code:
+```clojure
+;; We assume the stack is (7 6 5 4 3 2 1)
+(defn increment [x] (inc x))
+(defn addition [x y] (+ x y))
+
+[:!p increment] ;;=> (8 6 5 4 3 2 1)
+[:!p addition] ;;=> (13 5 4 3 2 1)
+
+increment ;;=> (8 7 6 5 4 3 2 1)
+addition ;;=> (13 7 6 5 4 3 2 1)
+```
+The p in :!p stands for "pop", which means "take the first value from the stack and remove it".
+At the end of each function call, the result gets put back.
+
+### Fork - If, the VisuFlow way
+When you need to decide which function to execute next, you need to fork your way.
+This is done using fork statements:
+```clojure
+[:!f #(inc %) :data]
+```
+The fork statement consists of three elements.
+1. The keyword to distinguish it: :!f means "fork", and :!fp means fork, but pop the values taken for the function from the stack.
+2. The validator: A function which gets applied just as the others in the data flow.
+3. A "validarg": A keyword telling VisuFlow how to interpret the result of the validator.
+
+#### Validargs
+There are some built-in validargs:
+:data "if input is a value other than true, false, nil"
+:true "if input is true"
+:false "if input is false"
+:nil "if input is nil"
+:fnil "if input is false or nil"
+:is "A vector query like above with custom checkers."	
+
+Furthermore you can define your own in the validargs-map you have to supply at the beginning:
+```clojure
+{:type [[map? 1]
+        [list? 2]
+	[vector? 3]
+	[:else 4]]}
+```
+It looks like a sub-flow. Each vector specifies an if-then statement.
+First, on the left side you can have anything that's a function and takes one argument,
+or :else.
+
+On the right side you have more options:
+1. Numbers: Tells the walker how much to drop from the list it's walking on. The walker can only ever work on the first element of the list. 0 means: Run the fork again.
+2. A keyword: Tells the walker: Drop one and continue at the list at the map value of the keyword.
+If no map is found, the walker will look if the first element of the vector is equal to the keyword.
+3. Chaining: You can chain numbers and keywords together in a sequence.
+e.g. [3 :foo] means: Drop three and continue as in 2., with the exception that no more entries will be dropped. :foo is an implicit [1 :foo].
+
+### More control - the :! colonbangs (unimplemented)
+```clojure
+:!p ;; *P*op instead of peek the values on the stack
+:!f ;; *F*ork the way
+:!fp ;; *F*ork the way, *p*op the stack
+:!_n ;; Clean the stack and only keep the last n values
+:!q ;; *Q*uit the flow and return the last result (NOT RECOMMENDED!)
+```
 
 ## License
 
-Copyright © 2013 FIXME
+Copyright © 2013 Daniel Ziltener
 
 Distributed under the Eclipse Public License, the same as Clojure.
--- src/visuflow/core.clj
+++ src/visuflow/core.clj
@@ -1,6 +1,201 @@
-(ns visuflow.core)
+(ns visiflow.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defmacro cond-let
+  "Takes a binding-form and a set of test/expr pairs. Evaluates each test
+  one at a time. If a test returns logical true, cond-let evaluates and
+  returns expr with binding-form bound to the value of test and doesn't
+  evaluate any of the other tests or exprs. To provide a default value
+  either provide a literal that evaluates to logical true and is
+  binding-compatible with binding-form, or use :else as the test and don't
+  refer to any parts of binding-form in the expr. (cond-let binding-form)
+  returns nil."
+  [bindings & clauses]
+  (let [binding (first bindings)]
+    (when-let [[test expr & more] clauses]
+      (if (= test :else)
+        expr
+        `(if-let [~binding ~test]
+           ~expr
+           (cond-let ~bindings ~@more))))))
+
+(defn arg-count [f]
+  (let [m (first (.getDeclaredMethods (class f)))
+        p (.getParameterTypes m)]
+    (alength p)))
+
+;; Input: any clojure data structure or thing
+;; It is built of clojure lists or vectors.
+
+;; Fundamental rule: A thing accepting one argument gets the last result,
+;; a thing accepting two arguments gets the two last ones, and so on, on a FIFO base.
+;; You can add the metadata-key :maxargs to define the max number of args for a vararg fun,
+;; or write it like [function maxargs].
+
+;; First entry in a vector:
+;; If it's a keyword, flow will apply it to the input.
+;; Then: When the input is a function it gets applyed, with the whole data structure as argument.
+;; After that, or if it's a data structure, it gets handed to the next thing in the list.
+
+;; If that's a function or keyword, it gets applyed.
+
+;; Takes input map valargs:
+;; Number says how many to drop. 1 means, drop validator expression, take next.
+{:st {:is [[map? 1]
+           [list? 2]
+           [:else 2]]}}
+
+;; Validarg types
+{:data "if input is a value other than true, false, nil"
+ :true "if input is true"
+ :false "if input is false"
+ :nil "if input is nil"
+ :fnil "if input is false or nil"
+ :is "A vector query like above with custom checkers."}
+
+;; Possible return values
+{1 "or any other number. How many entries to drop. 0 means evaluate the same expression again."
+ :foo "Continue at that key in the map after dropping 1."
+ :?foo "Continue at the vector where this is the first entry."
+ :?_ "Recommendation for default keyword in a vector."
+ [3 :foo] "Chaining. Numbers mean: drop. Keywords mean: :foo Key of a map :?foo \"key of a coll\""}
+
+(defn- v-data
+  [arg]
+  (if-not (or (nil? arg) (false? arg) (true? arg))
+    1 2))
+
+(defn- v-true
+  [arg]
+  (if (true? arg)
+    1 2))
+
+(defn- v-false
+  [arg]
+  (if (false? arg)
+    1 2))
+
+(defn- v-nil
+  [arg]
+  (if (nil? arg)
+    1 2))
+
+(defn- v-fnil
+  [arg]
+  (if (or (false? arg) (nil? arg))
+    1 2))
+
+(defn v-is
+  [arg validarg-list]
+  (reduce (fn [rval [fun ret]]
+            (if (= :else fun)
+              ret
+              (if (true? (apply fun [arg]))
+                ret
+                rval)))
+          nil
+          validarg-list))
+
+(defn- popstack
+  [{:keys [stack count]}]
+  [(take count stack) (drop count stack)])
+
+(defn- getstack
+  [{:keys [stack count]}]
+  [(take count stack) stack])
+
+(defn- apply-validarg
+  [{:keys [inp varg validargs]}]
+  (case varg
+    :data (v-data inp)
+    :true (v-true inp)
+    :false (v-false inp)
+    :nil (v-nil inp)
+    :fnil (v-fnil inp)
+    (if-let [is-varg (varg validargs)]
+      (v-is inp is-varg)
+      (throw (Exception. (str "Validarg does not exist: " varg " in " validargs))))))
+
+(defn- eval-with-stack
+  [{:keys [pop? car args stack]}]
+  (let [arity (if (nil? args)
+                (arg-count car)
+                (- (arg-count car) (count args)))
+        [stackargs stack] (if pop?
+                            (popstack {:stack stack :count arity})
+                            (getstack {:stack stack :count arity}))
+        result (if (nil? args)
+                 (apply car stackargs)
+                 (apply car (flatten (conj [] args stackargs))))]
+    (conj stack result)))
+
+(defn- parse-validarg-stmt "Parses one validarg statement and applies it to the coll."
+  [coll stmt]
+  (cond
+   (integer? stmt)
+   (drop stmt coll)
+   
+   (keyword? stmt)
+   (->> coll (drop 1) stmt)
+   
+   :else
+   coll))
+
+(defn- parse-validarg-res "Coordinates parsing of a list of validarg statements."
+  [coll [car & cdr]]
+  (if (or (empty? cdr) (nil? cdr))
+    (if (nil? car)
+      coll
+      (parse-validarg-stmt coll car))
+    (parse-validarg-res (parse-validarg-stmt coll car) cdr)))
+
+(defn- eval-fork "Evaluates a fork expression"
+  [{:keys [coll vargs stack]}]
+  (let [fork (first coll)
+        funstruct (eval (second fork))
+        fun (if (fn? funstruct) funstruct (first funstruct))
+        funargs (if (fn? funstruct) nil (rest funstruct))
+        validarg (last fork)
+        stack (eval-with-stack {:pop? (= :!fp (first fork)) :car fun :args funargs :stack stack})
+        result (first stack)
+        vargres (apply-validarg {:inp result :varg validarg :validargs vargs})]
+    [(parse-validarg-res vargres) (conj stack result)]))
+
+(defn- eval-elem
+  [{:keys [coll vargs stack]}]
+  (let [car (first coll)
+        arg (first stack)]
+    (cond
+     (and (keyword? car) (.startsWith (name car) ":!"))
+     [coll stack]
+     
+     (or (keyword? car) (fn? car))
+     [(drop 1 coll) (eval-with-stack :car car :stack stack)]
+     
+     (sequential? car)
+     (if (or (= :!f (first car)) (= :!fp (first car)))
+       (eval-fork :coll coll :vargs vargs :stack stack)
+       
+       (let [pop? (= :!p (first car))
+             cdr (if pop? (rest car) car)
+             fun (first cdr)
+             args (rest cdr)]
+        [(drop 1 coll) (eval-with-stack :pop? pop? :car fun :args args :stack stack)]))
+     
+     :else ;; TODO broken
+     [(drop 1 coll) (conj stack (arg car))])))
+
+(defn walk
+  ([{:keys [state tree validargs]}]
+     (walk true :tree tree :validargs validargs :stack (conj (list) state)))
+  
+  ([x {:keys [tree validargs stack]}]
+     (cond
+      (list? (first tree))
+      (walk true :tree (first tree) :validargs validargs :stack stack)
+      
+      (empty? (rest tree))
+      (eval-elem :coll tree :vargs validargs :stack stack)
+      
+      :else
+      (let [[tree stack] (eval-elem :coll tree :vargs validargs :stack stack)]
+        (walk true :tree tree :validargs validargs :stack stack)))))
